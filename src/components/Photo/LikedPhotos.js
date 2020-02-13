aeb75857bf1e14537fe5ebacda8bc794aa04c6ed
sookim-Boston <soojeong914@gmail.com>
2020-02-13 16:42:38 -0500

    Add delete button and styling
    - Fix the picture size
    - Add delete button and axios call to delete the photo from database

--- src/components/Photo/LikedPhotos.js
+++ src/components/Photo/LikedPhotos.js
@@ -3,8 +3,25 @@ import { withRouter } from 'react-router-dom'
 import apiUrl from '../../apiConfig'
 import axios from 'axios'
 import Img from 'react-image'
+import Button from 'react-bootstrap/Button'
 
-const LikedPhotos = ({ user, match, alert }) => {
+const imgSize = {
+  width: '250px',
+  height: '250px',
+  margin: '10px',
+  display: 'block'
+}
+
+const divStyle = {
+  float: 'left',
+  textAlign: 'center'
+}
+
+const font = {
+  fontFamily: 'Delius'
+}
+
+const LikedPhotos = ({ user, match, alert, history }) => {
   const [likedPhotos, setlikedPhotos] = useState([])
 
   useEffect(() => {
@@ -21,16 +38,38 @@ const LikedPhotos = ({ user, match, alert }) => {
       .catch(() => alert({ heading: 'Rut roh', message: 'Something went wrong', variant: 'danger' }))
   }, [])
 
+  const destroy = (id) => {
+    event.preventDefault()
+    axios({
+      url: `${apiUrl}/liked-photos/${id}`,
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+      .then(() => alert({ zIndex: 1, heading: 'Success', message: 'Removed!', variant: 'success' }))
+      .then(() => {
+        history.replace('/reload')
+        history.replace('/liked-photos')
+      })
+      .catch(() => alert({ heading: 'Rut roh', message: 'Something went wrong', variant: 'danger' }))
+  }
+
   if (!likedPhotos) {
     return <p>Loading...</p>
   }
 
-  const photosJsx = likedPhotos.map(photo =>
-    <Img key={photo._id} src={photo.url} />
-  )
+  const photosJsx = likedPhotos.map(photo => (
+    <div key={photo._id} style={divStyle} >
+      <Img style={imgSize} src={photo.url} />
+      <Button onClick={() => destroy(photo._id)} style={font} variant='outline-warning'>Remove</Button>
+    </div>
+  ))
 
   return (
-    <Fragment>{photosJsx}</Fragment>
+    <Fragment>
+      {photosJsx}
+    </Fragment>
   )
 }
 
