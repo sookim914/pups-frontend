2ae08286102e12c9d2857ff8d85b599e35f72f43
sookim-Boston <soojeong914@gmail.com>
2020-02-13 14:51:31 -0500

    Add styling
    - Add emoji next to the brand
    - Changed fonts and size and color
    - Changed the Navbar bg color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,33 @@
 import React, { Fragment } from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
+import Emoji from 'react-emoji-render'
+
+const title = {
+  color: '#343a40',
+  fontSize: '25px',
+  fontFamily: 'Delius'
+}
+
+const font = {
+  fontFamily: 'Delius',
+  fontSize: '16px',
+  color: '#343a40'
+}
 
 const authenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#get-photos">Puppies</Nav.Link>
-    <Nav.Link href="#liked-photos">Favorites</Nav.Link>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <Nav.Link href="#get-photos" style={font}>Puppies</Nav.Link>
+    <Nav.Link href="#liked-photos" style={font}>Favorites</Nav.Link>
+    <Nav.Link href="#change-password" style={font}>Change Password</Nav.Link>
+    <Nav.Link href="#sign-out" style={font}>Sign Out</Nav.Link>
   </Fragment>
 )
 
 const unauthenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#sign-up">Sign Up</Nav.Link>
-    <Nav.Link href="#sign-in">Sign In</Nav.Link>
+    <Nav.Link href="#sign-up" style={font} >Sign Up</Nav.Link>
+    <Nav.Link href="#sign-in" style={font} >Sign In</Nav.Link>
   </Fragment>
 )
 //
@@ -25,13 +38,13 @@ const unauthenticatedOptions = (
 // )
 
 const Header = ({ user }) => (
-  <Navbar bg="primary" variant="dark" expand="md">
-    <Navbar.Brand href="#">
-      PUPS
+  <Navbar bg="light" variant="dark" expand="md">
+    <Navbar.Brand href="#" style={title}>
+      PUPS <Emoji size={50} text=':paw_prints:'/>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
+      <Nav style={font} className="ml-auto">
         { user && <span className="navbar-text mr-2">Wuff, {user.email}</span>}
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
